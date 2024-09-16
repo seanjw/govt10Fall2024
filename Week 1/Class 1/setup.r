3affa3f0577ab9a365154eee5dbb3629b6e35959
Sean J. Westwood <sean.j.westwood@dartmouth.edu>
2024-09-16 17:07:56 -0400

    week1

---
+++ Week 1/Class 1/setup.r
@@ -0,0 +1,12 @@
+install.packages("formatR")
+install.packages('tinytex')
+tinytex::install_tinytex()
+install.packages('knitr')
+install.packages('DiagrammeR')
+install.packages("webshot2")
+library(webshot2)
+webshot2::install_phantomjs()
+# https://github.com/seanjw/GOVT10Winter2024
+
+
+
